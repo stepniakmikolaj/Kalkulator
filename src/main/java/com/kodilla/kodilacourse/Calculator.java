d905c333b79edf4aa197ca2ad1bd7d0b5758962c
stepniakmikolaj <stepniakmikolaj@gmail.com>
2018-01-03 12:43:24 +0100

    first commit

---
+++ src/main/java/com/kodilla/kodilacourse/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilacourse;
+
+public class Calculator {
+
+    public void someMethod() {
+        System.out.println("It works!");
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
