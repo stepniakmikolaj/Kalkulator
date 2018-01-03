220400ed8bae8dea399272186db83b36be362e85
stepniakmikolaj <stepniakmikolaj@gmail.com>
2018-01-03 21:51:10 +0100

    Third Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+public class Calculator {
+    public void someMethod() {
+        System.out.println("It works!");
+    }
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
+
+
